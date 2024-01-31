d29786dbbf492d6496874e03baf775a652b08f76
Iveta Eliášková <eliaskova.i@gmail.com>
2024-01-31 20:35:20 +0100

    hod kostkou

--- script_1.js
+++ script_1.js
@@ -4,5 +4,11 @@
 Zamyslete se nad tím, zda vámi vytvořený výraz generuje všechna čísla skutečně se stejnou pravděpodobností.
 Vemte v úvahu, že funkce Math.random generuje náhodná čísla mezi 0 (včetně) a 1 (vyjma). Je tedy malinká pravěpodobnost, že občas padne přesně číslo 0. Naopak číslo 1 padnout nemůže.*/
 
+pocet_hodu = 1
+steny = 6
+var cislo = 0;
+for(var i=0;i<pocet_hodu;i++){
+   cislo += Math.round (steny*Math.random()+1);
+}
 
-
+console.log(cislo)
